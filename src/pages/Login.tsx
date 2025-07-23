0f2a2af36feebf22dde7a2fa07cb9fe8be97b8af
Thomas Dascalu <thomasdascalu@Mac-mini-di-Thomas.local>
2025-07-23 13:48:04 +0200

    prova fix non passaggio a dashboard

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,20 +4,9 @@ import './Login.css';
 
 
 
-function parseJwt(token: string) {
-  try {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-        .join('')
-    );
-    return JSON.parse(jsonPayload);
-  } catch {
-    return null;
-  }
+function parseJwt(t: string) {
+  try { return JSON.parse(atob(t.split('.')[1])); }
+  catch { return null; }
 }
 
 
